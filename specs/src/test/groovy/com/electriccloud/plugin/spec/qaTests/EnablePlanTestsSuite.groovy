03248393951e5584f2cfdc3facabd7db944c0c25
Ed Serbin <eserbin@cloudbees.com>
2019-06-29 22:11:52 +0300

    added tests for Enable / Disable plan procedures

--- specs/src/test/groovy/com/electriccloud/plugin/spec/qaTests/EnablePlanTestsSuite.groovy
+++ specs/src/test/groovy/com/electriccloud/plugin/spec/qaTests/EnablePlanTestsSuite.groovy
@@ -5,11 +5,9 @@ import com.electriccloud.plugin.spec.PluginTestHelper
 import com.electriccloud.plugin.spec.TestCaseHelper
 import com.electriccloud.plugins.annotations.NewFeature
 import com.electriccloud.plugins.annotations.Sanity
-import groovy.json.JsonSlurper
-import spock.lang.IgnoreRest
 import spock.lang.Unroll
 
-class EnablePLanTestsSuite extends PluginTestHelper{
+class EnablePlanTestsSuite extends PluginTestHelper{
 
     static def procedureName = 'EnablePlan'
 
@@ -63,21 +61,119 @@ class EnablePLanTestsSuite extends PluginTestHelper{
         dslFile "dsl/procedure.dsl", [projectName: projectName, resName: 'local', procedureName: 'EnablePlan', params: runPlanParams]
 
         bambooClient = new BambooClient('http', commanderAddress,  '8085', '', BAMBOO_USERNAME, BAMBOO_PASSWORD)
-//        bambooClient.createPlan('PROJECT', 'QAENABLEPLAN', 'Enable plan')
+        bambooClient.createPlan('PROJECT', 'QAENABLEPLAN', 'Enable plan')
 
     }
 
     def doCleanupSpec() {
         testCaseHelper.createTestCases()
-//        deleteConfiguration(PLUGIN_NAME, CONFIG_NAME)
-//        conditionallyDeleteProject(projectName)
-//        bambooClient.deletePlan('PROJECT', 'QAENABLEPLAN')
+        deleteConfiguration(PLUGIN_NAME, CONFIG_NAME)
+        conditionallyDeleteProject(projectName)
+        bambooClient.deletePlan('PROJECT', 'QAENABLEPLAN')
+    }
+
+    @Sanity
+    @Unroll
+    def 'GetPlanRuns: Sanity #caseId.ids #caseId.description'() {
+        if (planState == 'disabled') {
+            bambooClient.changePlanState(projectKey, planKey, 'disable')
+        }
+
+        given: "Tests parameters for procedure"
+        def runParams = [
+                config                  : configName,
+                projectKey              : projectKey,
+                planKey                 : planKey,
+        ]
+
+        when: "Run procedure"
+
+
+        def result = runProcedure(projectName, procedureName, runParams)
+        def jobSummary = getStepSummary(result.jobId, procedureName)
+
+        def planInfo = bambooClient.getPlanDetails(projectKey, planKey)
+
+        then: "Verify results"
+        assert result.outcome == expectedOutcome
+
+        assert jobSummary == expectedSummary
+                .replace("PROJECTKEY", projectKey)
+                .replace("PLANKEY", planKey)
+
+        assert planInfo.enabled == true
+
+        for (def log in expectedLog) {
+            assert result.logs.contains(log
+                    .replace("PROJECTKEY", projectKey)
+                    .replace("PLANKEY", planKey))
+        }
+
+        where:
+        caseId     | configName   | projectKey     | planKey         | expectedOutcome | planState | expectedSummary             | expectedLog
+        TC.C388135 | CONFIG_NAME  | 'PROJECT'      | 'QAENABLEPLAN'  | 'success'       | 'disabled'| expectedSummaries.default   | expectedLogs.default
     }
 
 
     @NewFeature(pluginVersion = "1.5.0")
     @Unroll
     def 'GetPlanRuns: Positive #caseId.ids #caseId.description'() {
+        testCaseHelper.createNewTestCase(caseId.ids, caseId.description)
+        testCaseHelper.testCasePrecondition('Plan QAENABLEPLAN should exist in project PROJECT')
+
+        if (planState == 'disabled') {
+            testCaseHelper.testCasePrecondition('Plan QAENABLEPLAN should be disabled')
+            bambooClient.changePlanState(projectKey, planKey, 'disable')
+        }
+        else{
+            testCaseHelper.testCasePrecondition('Plan QAENABLEPLAN should be enabled')
+        }
+
+
+        given: "Tests parameters for procedure"
+        def runParams = [
+                config                  : configName,
+                projectKey              : projectKey,
+                planKey                 : planKey,
+        ]
+
+        when: "Run procedure"
+
+        testCaseHelper.addStepContent("Run procedure $procedureName with parameters:", runParams)
+
+        def result = runProcedure(projectName, procedureName, runParams)
+        def jobSummary = getStepSummary(result.jobId, procedureName)
+
+        def planInfo = bambooClient.getPlanDetails(projectKey, planKey)
+
+        then: "Verify results"
+        testCaseHelper.addExpectedResult("Job status: $expectedOutcome")
+        assert result.outcome == expectedOutcome
+
+        testCaseHelper.addExpectedResult("Job Summary: ${expectedSummary.replace("PROJECTKEY", projectKey).replace("PLANKEY", planKey)}")
+        assert jobSummary == expectedSummary
+                .replace("PROJECTKEY", projectKey)
+                .replace("PLANKEY", planKey)
+
+        testCaseHelper.addExpectedResult("Bamboo plan should be enabled")
+        assert planInfo.enabled == true
+
+        for (def log in expectedLog) {
+            testCaseHelper.addExpectedResult("Job logs contains: $log")
+            assert result.logs.contains(log
+                    .replace("PROJECTKEY", projectKey)
+                    .replace("PLANKEY", planKey))
+        }
+
+        where:
+        caseId     | configName   | projectKey     | planKey         | expectedOutcome | planState | expectedSummary             | expectedLog
+        TC.C388135 | CONFIG_NAME  | 'PROJECT'      | 'QAENABLEPLAN'  | 'success'       | 'disabled'| expectedSummaries.default   | expectedLogs.default
+        TC.C388135 | CONFIG_NAME  | 'PROJECT'      | 'QAENABLEPLAN'  | 'success'       | 'enabled' | expectedSummaries.default   | expectedLogs.default
+    }
+
+    @NewFeature(pluginVersion = "1.5.0")
+    @Unroll
+    def 'GetPlanRuns: Negative #caseId.ids #caseId.description'() {
         testCaseHelper.createNewTestCase(caseId.ids, caseId.description)
         testCaseHelper.testCasePrecondition('Plan QAENABLEPLAN should exist in project PROJECT')
 
@@ -95,16 +191,17 @@ class EnablePLanTestsSuite extends PluginTestHelper{
         def result = runProcedure(projectName, procedureName, runParams)
         def jobSummary = getStepSummary(result.jobId, procedureName)
 
-        def outputParameters = getJobOutputParameters(result.jobId, 1)
-        def jobProperties = getJobProperties(result.jobId)
-
-
         then: "Verify results"
         testCaseHelper.addExpectedResult("Job status: $expectedOutcome")
         assert result.outcome == expectedOutcome
 
-        testCaseHelper.addExpectedResult("Job Summary: ${expectedSummary.replace('PLANRUNKEY', lastPlanRunName)}")
-        assert jobSummary == expectedSummary.replace('PLANRUNKEY', lastPlanRunName)
+        if (expectedSummary) {
+            testCaseHelper.addExpectedResult("Job Summary: ${expectedSummary.replace("PROJECTKEY", projectKey).replace("PLANKEY", planKey)}")
+            assert jobSummary == expectedSummary
+                    .replace("PROJECTKEY", projectKey)
+                    .replace("PLANKEY", planKey)
+        }
+
 
         for (def log in expectedLog) {
             testCaseHelper.addExpectedResult("Job logs contains: $log")
@@ -114,9 +211,14 @@ class EnablePLanTestsSuite extends PluginTestHelper{
         }
 
         where:
-        caseId     | configName   | projectKey     | planKey         | expectedOutcome | expectedSummary             | expectedLog
-        TC.C388135 | CONFIG_NAME  | 'PROJECT'      | 'QAENABLEPLAN'  | 'success'       | expectedSummaries.default   | expectedLogs.default
-    }
+        caseId     | configName   | projectKey     | planKey         | expectedOutcome | planState | expectedSummary             | expectedLog
+        TC.C388135 | ''           | 'PROJECT'      | 'QAENABLEPLAN'  | 'error'         | 'disabled'| null                        | expectedLogs.defaultError
+        TC.C388135 | 'wrong'      | 'PROJECT'      | 'QAENABLEPLAN'  | 'error'         | 'disabled'| null                        | expectedLogs.defaultError
+        TC.C388135 | CONFIG_NAME  | ''             | 'QAENABLEPLAN'  | 'error'         | 'disabled'| null                        | expectedLogs.defaultError
+        TC.C388135 | CONFIG_NAME  | 'PROJECT'      | ''              | 'error'         | 'disabled'| null                        | expectedLogs.defaultError
+        TC.C388135 | CONFIG_NAME  | 'WRONG'        | 'QAENABLEPLAN'  | 'error'         | 'disabled'| expectedSummaries.notFound  | expectedLogs.notFound
+        TC.C388135 | CONFIG_NAME  | 'PROJECT'      | 'WRONG'         | 'error'         | 'disabled'| expectedSummaries.notFound  | expectedLogs.notFound
 
+    }
 
 }
